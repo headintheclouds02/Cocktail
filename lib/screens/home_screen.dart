f9f2abbbe411f7fa3837aedec818e63f01a8695f
headintheclouds02 <carolacocco02@gmail.com>
2025-12-16 14:02:49 +0100

    search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_cocktail/components/custom_tapbar.dart';
+import 'package:flutter/material.dart' hide SearchBar;
+import 'package:flutter_svg/flutter_svg.dart';
 import '../components/custom_button.dart';
+import '../components/search_bar.dart';
+import '../theme/app_colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,21 +12,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _State extends State<HomeScreen> {
-  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Benvenuto!', style: TextStyle(fontFamily: 'Gabarito', fontSize: 32)),
-        centerTitle: false,
-      ),
-      body:
-
-      ListView(
-        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-        children: [
-          CustomButton(text: 'Crea il tuo cocktail', onPressed: () {}),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Benvenuto!', style: TextStyle(fontFamily: 'Gabarito', fontSize: 32)),
+          centerTitle: false,
+        ),
+        body: ListView(
+          children: [
+            CustomSearchBar(hintText: ('Cosa vuoi bere?'), onChanged: (value) {}, icon: SvgPicture.asset('assets/img/icone/search.svg', width: 20, height: 20, color: AppColors.iconFocused)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: CustomButton(text: 'Crea il tuo cocktail', onPressed: () {}),
+            ),
+            ],
+        ),
       ),
     );
   }
