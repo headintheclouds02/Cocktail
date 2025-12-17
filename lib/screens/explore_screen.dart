0f9c5b3cfdd1017f0698784f7eba6456e16a26d5
headintheclouds02 <carolacocco02@gmail.com>
2025-12-17 09:49:46 +0100

    aggiornata explore screen

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../components/cocktail_card.dart';
+import '../theme/app_colors.dart';
+
 class ExploreScreen extends StatefulWidget {
   const ExploreScreen({super.key});
 
@@ -10,6 +13,39 @@ class ExploreScreen extends StatefulWidget {
 class _ExploreScreenState extends State<ExploreScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Esplora',
+            style: TextStyle(fontFamily: 'Gabarito', fontSize: 32),
+          ),
+          centerTitle: false,
+        ),
+        body: InkWell(
+          onTap: () {
+            // Azione al tap
+          },
+          child: GridView.count(
+            crossAxisCount: 3,
+            // Numero di colonne
+            padding: EdgeInsets.all(10),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            children: List.generate(20, (index) {
+              return Container(
+                color: Colors.blueAccent,
+                child: Center(
+                  child: Text(
+                    'Item $index',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      ),
+    );
   }
 }
