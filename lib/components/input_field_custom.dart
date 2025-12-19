171c0f736e7393e8d374febb938a5eb827e35418
headintheclouds02 <carolacocco02@gmail.com>
2025-12-19 15:26:15 +0100

    schermata login ui finita

---
+++ lib/components/input_field_custom.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../theme/app_colors.dart';
+
+class InputFieldCustom extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+  final String hintText;
+  final TextEditingController? controller;
+  final SvgPicture icon;
+  final bool hideText;
+  final Widget? suffixIcon;
+
+
+
+  const InputFieldCustom({
+    super.key,
+    required this.onChanged,
+    required this.hintText,
+    this.controller,
+    required this.icon,
+    required this.hideText,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        color: AppColors.searchBar,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: TextField(
+        controller: controller,
+        onChanged: onChanged,
+        obscureText: hideText,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText,
+          icon: icon,
+          suffixIcon: suffixIcon,
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cocktail/components/input_field_custom.dart';
+import 'package:flutter_cocktail/screens/main_page.dart';
 import 'package:flutter_cocktail/screens/registration_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../components/custom_button.dart';
+import '../components/text_field.dart';
 import '../theme/app_colors.dart';
+import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,152 +17,172 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isPasswordHidden = true;
+
   @override
   Widget build(BuildContext context) {
-    //return Scaffold(
-    //  backgroundColor: AppColors.tapBarBackground,
-    //
-    //  appBar: AppBar(
-    //    title: Row(
-    //      mainAxisAlignment: MainAxisAlignment.center,
-    //      children: [
-    //        Padding(
-    //          padding: const EdgeInsets.symmetric(horizontal: 4),
-    //          child: SizedBox(
-    //            width: 45,
-    //            height: 45,
-    //            child: Image.asset("assets/img/generic/logo_wht_nobg.png"),
-    //          ),
-    //        ),
-    //        Padding(
-    //          padding: const EdgeInsets.symmetric(horizontal: 4),
-    //          child: Text(
-    //            "Bevo, quindi sono",
-    //            style: TextStyle(fontFamily: 'Gabarito', fontSize: 22),
-    //          ),
-    //        ),
-    //      ],
-    //    ),
-    //    backgroundColor: AppColors.tapBarBackground,
-    //    //leading: IconButton(
-    //    //  onPressed: () {Navigator.pop(context);},
-    //    //  icon: Icon(Icons.arrow_back, color: AppColors.iconUnfocused),
-    //    //),
-    //  ),
-    //  body: Column(
-    //    children: [
-    //      Positioned(
-    //        child: Image.asset("assets/img/generic/login.png", height: 400),
-    //        bottom: 0,
-    //        right: -30,
-    //      ),
-    //      Padding(
-    //        padding: const EdgeInsets.symmetric(horizontal: 32),
-    //        child: Stack(
-    //          children: [
-//
-//
-//
-//
-    //            Container(
-    //              width: double.infinity,
-//
-    //              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-//
-    //              decoration: BoxDecoration(
-    //                borderRadius: BorderRadius.circular(25),
-    //                color: AppColors.iconUnfocused.withOpacity(0.8),
-    //              ),
-    //              child: Column(
-    //                children: [
-    //                  Text(
-    //                    "Login",
-    //                    style: TextStyle(fontFamily: 'Gabarito', fontSize: 32),
-    //                  ),
-//
-    //                  Text(
-    //                    "Email",
-    //                    style: TextStyle(fontFamily: 'Gabarito', fontSize: 20),
-    //                  ),
-    //                ],
-    //              ),
-    //            ),
-//
-    //            Spacer(),
-//
-    //            CustomButton(
-    //              text: "Login",
-    //              onPressed: () {
-    //                Navigator.push(
-    //                  context,
-    //                  MaterialPageRoute(builder: (context) => LoginScreen()),
-    //                );
-    //              },
-    //            ),
-//
-    //            TextButton(
-    //              onPressed: () {
-    //                Navigator.push(
-    //                  context,
-    //                  MaterialPageRoute(builder: (context) => RegistrationScreen()),
-    //                );
-    //              },
-    //              child: Padding(
-    //                padding: const EdgeInsets.all(16.0),
-    //                child: Text(
-    //                  "o crea un account",
-    //                  style: TextStyle(
-    //                    fontFamily: 'Gabarito',
-    //                    fontSize: 20,
-    //                    color: AppColors.buttonEnabled,
-    //                    decoration: TextDecoration.underline,
-    //                  ),
-    //                ),
-    //              ),
-    //            ),
-//
-    //            Spacer(),
-    //          ],
-    //        ),
-    //      ),
-    //    ]
-//
-//
-    //  )
-//
-    //);
-
     return Scaffold(
       backgroundColor: AppColors.tapBarBackground,
 
-        appBar: AppBar(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: SizedBox(
-                      width: 45,
-                      height: 45,
-                      child: Image.asset("assets/img/generic/logo_wht_nobg.png"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: Text(
-                      "Bevo, quindi sono",
-                      style: TextStyle(fontFamily: 'Gabarito', fontSize: 22),
-                    ),
-                  ),
-                ],
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: SizedBox(
+                width: 45,
+                height: 45,
+                child: Image.asset("assets/img/generic/logo_wht_nobg.png"),
               ),
-              backgroundColor: AppColors.tapBarBackground,
-              //leading: IconButton(
-              //  onPressed: () {Navigator.pop(context);},
-              //  icon: Icon(Icons.arrow_back, color: AppColors.iconUnfocused),
-              //),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: Text(
+                "Bevo, quindi sono",
+                style: TextStyle(fontFamily: 'Gabarito', fontSize: 22),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: AppColors.tapBarBackground,
+      ),
 
+      body: Stack(
+        children: [
+          //livello 1
+          Positioned(
+            bottom: 0,
+            right: -55,
+            child: Image.asset("assets/img/generic/login.png", height: 500),
+          ),
+
+          //livello 2
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 80),
+            child: Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: AppColors.iconUnfocused.withOpacity(0.8),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    children: [
+                      Spacer(),
+                      Text(
+                        "Login",
+                        style: TextStyle(fontFamily: 'Gabarito', fontSize: 32),
+                      ),
+                      Spacer(),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            "Username",
+                            style: TextStyle(
+                              fontFamily: 'Gabarito',
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      InputFieldCustom(
+                        onChanged: (String value) {},
+                        hintText: 'Inserisci username',
+                        icon: SvgPicture.asset("assets/img/generic/user.svg"),
+                        hideText: false,
+                      ),
+
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(
+                              fontFamily: 'Gabarito',
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                      InputFieldCustom(
+                        onChanged: (String value) {},
+                        hintText: 'Inserisci password',
+                        hideText: isPasswordHidden,
+                        icon: SvgPicture.asset(
+                          "assets/img/generic/password.svg",
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isPasswordHidden = !isPasswordHidden;
+                            });
+                          },
+                          icon: SvgPicture.asset(
+                            isPasswordHidden
+                                ? "assets/img/generic/hide.svg"
+                                : "assets/img/generic/show.svg",
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      CustomButton(
+                        text: "Mixiamo!",
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MainPage(),
+                            ),
+                            (route) => false,
+                          );
+                        },
+                      ),
+                      Spacer(),
+                      Text(
+                        'Prima volta al bancone?',
+                        style: TextStyle(fontFamily: 'Gabarito', fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegistrationScreen(),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 2),
+                          child: Text(
+                            "Unisciti a noi",
+                            style: TextStyle(
+                              fontFamily: 'Gabarito',
+                              fontSize: 20,
+                              color: AppColors.buttonEnabled,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
